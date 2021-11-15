6296d43889f2d4c3bb4f83e7d162eb18f5a7f2a5
DESKTOP-1VP9R0T\sny2s <sny2sd@gmail.com>
2021-11-15 22:14:11 +0900

    表示内容の変更

--- Programming_CSharp/Programming_CSharp/Program.cs
+++ Programming_CSharp/Programming_CSharp/Program.cs
@@ -6,7 +6,7 @@ namespace Programming_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("yoshida");
         }
     }
 }
