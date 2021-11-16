f48b97c99ca0644964676a7d6dba18c06f1dbb4c
DESKTOP-1VP9R0T\sny2s <sny2sd@gmail.com>
2021-11-16 12:22:20 +0900

    aaaa

--- Programming_CSharp/Programming_CSharp/Program.cs
+++ Programming_CSharp/Programming_CSharp/Program.cs
@@ -6,7 +6,36 @@ namespace Programming_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("tokuda");
+            Console.WriteLine("名前を入力してください");
+            var userName = Console.ReadLine();
+            Console.WriteLine("パスワードを入力してください");
+            var userPassword = Console.ReadLine();
+            int i = 0;
+
+
+            do
+            {
+
+                Console.WriteLine("もう一度名前を入力してください");
+                var userNameVerify = Console.ReadLine();
+                Console.WriteLine("もう一度パスワードを入力してください");
+                var userPasswordVerify = Console.ReadLine();
+
+                if (userName != userNameVerify)
+                {
+                    Console.WriteLine("名前が間違っています");
+                }
+                else if (userPassword != userPasswordVerify)
+                {
+                    Console.WriteLine("パスワードが間違っています");
+                }
+                else
+                {
+                    Console.WriteLine("ログイン成功");
+                    i = 1;
+                }
+
+            } while (i == 0);
         }
     }
 }
