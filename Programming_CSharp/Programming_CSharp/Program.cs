1de7ac7f9d357615232da31eddbb254c2fc7289e
DESKTOP-1VP9R0T\sny2s <sny2sd@gmail.com>
2021-11-29 22:22:47 +0900

    jsonFile読み込み

--- Programming_CSharp/Programming_CSharp/Program.cs
+++ Programming_CSharp/Programming_CSharp/Program.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.IO;     //ファイル操作
 using System.Text;　 //高度な文字処理
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 
 namespace Programming_CSharp
 {
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var writer = new StreamWriter(@"C:\Programming\SRC\Programming_CSharp\Programming_CSharp\Test.txt",false);
+            var fileName = @"C:\Programming\SRC\Programming_CSharp\Programming_CSharp\test.json";
+            string jsonString = File.ReadAllText(fileName);
+            Person person = JsonSerializer.Deserialize<Person>(jsonString);
 
-            writer.WriteLine("この内容を書き込みます。");
-
-            writer.Close();
-
+            Console.WriteLine($"Name: {person.Name}");
+            Console.WriteLine($"Age: {person.Age}");
+            Console.WriteLine($"Country: {person.Country}");
         }
     }
 }
